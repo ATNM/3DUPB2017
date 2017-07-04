2a7d85ceeb8e3ed685efd96f90d9d9024bc46ad7
“Adrian <apetrosanu@airtouchmedia.com>
2017-07-04 14:48:26 +0300

    Lab 2

---
+++ Lab2/Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(WanderingAI))]
+public class ReactiveTarget : MonoBehaviour {
+
+	public void ReactToHit() {
+		WanderingAI wand = GetComponent<WanderingAI>();
+		wand.Unalive();
+		StartCoroutine(Die());
+	}
+
+	private IEnumerator Die() {
+		this.transform.Rotate(-75, 0, 0);
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Lab2/Assets/Scripts/WanderingAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float obstacleRange = 3.0f;
+	private bool _alive;
+
+	void Start () {
+		_alive = true;
+	}
+	
+	void Update () {
+		if (_alive) {
+			transform.Translate(0, 0, speed * Time.deltaTime);
+			Ray ray = new Ray(transform.position, transform.forward);
+			RaycastHit hit;
+			if(Physics.SphereCast(ray, 0.75f, out hit)) {
+				// if(hit.distance < obstacleRange) {
+				// 	float angle = Random.Range(-100, 100);
+				// 	transform.Rotate(0, angle, 0);
+				// }
+
+				GameObject hitObject = hit.transform.gameObject;
+				if(hitObject.GetComponent<PlayerCharacter>()) {
+					// TODO: FIREEEEEEEEEEEEEEEEEEE
+				}
+				else if(hit.distance < obstacleRange) {
+					float angle = Random.Range(-100, 100);
+					transform.Rotate(0, angle, 0);
+				}
+			}
+		}
+	}
+
+	public void Unalive() {
+		_alive = false;
+	}
+}
