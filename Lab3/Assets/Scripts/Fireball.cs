bbc66cf3749ac0daf31501bd4cf0fd52226184b4
“Adrian <apetrosanu@airtouchmedia.com>
2017-07-07 14:49:27 +0300

    Lab 3

---
+++ Lab3/Assets/Scripts/Fireball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+	public float spped = 10.0f;
+	public int damage = -1;
+	
+	void Update () {
+		transform.Translate(0, 0, spped * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		PlayerCharacter p = other.GetComponent<PlayerCharacter>();
+		if (p != null) {
+			p.Hurt(damage);
+		}
+
+		Destroy(this.gameObject);
+	}
+}
