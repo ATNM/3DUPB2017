871be1a5730759669983ebbe78591fc8b76be1fc
“Adrian <apetrosanu@airtouchmedia.com>
2017-07-19 09:10:14 +0300

    Lab5

---
+++ Lab5/Assets/Gigi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Gigi : MonoBehaviour {
+
+	PlayableDirector dir;
+
+	// Use this for initialization
+	void Start () {
+		dir = GetComponent<PlayableDirector>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			dir.Play();
+		}
+	}
+}
