e2d17558940b5da5d745e5cd9cf4534923246e0c
“Adrian <apetrosanu@airtouchmedia.com>
2017-07-20 09:18:20 +0300

    Lab6

---
+++ Lab6/Assets/Vasile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vasile : MonoBehaviour {
+
+	public void OnPEnter() {
+		Debug.Log("SARAMURA");
+	}
+
+	public void OnPExit() {
+		Debug.Log("MURATURA");
+	}
+}
